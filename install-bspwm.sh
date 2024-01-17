7ce5ad4e6b2190e67e67e2ad98936c8196bbcc60
Ltlis <156871608+Ltlis@users.noreply.github.com>
2024-01-17 16:19:12 -0300

    Update install-bspwm.sh

--- install-bspwm.sh
+++ install-bspwm.sh
@@ -22,7 +22,7 @@ cd $HOME
 # needed packages
 echo "Installing packages"
 sleep 1
-yay -S --noconfirm bspwm nvidia amd-ucode gcc gvfs gvfs-mtp htop imagemagick kitty neofetch mpv lxappearance polkit-gnome qbittorrent rofi swappy dunst thunar thunar-volman thunar-archive-plugin fileroller unrar unzip  webp-pixbuf-loader pipewire wireplumber pipewire-pulse xclip yt-dlp zsh steam ttf-jetbrains-mono-nerd man pavucontrol firefox plocate tumbler feh bat eza starship picom-ftlabs-git 
+yay -S --noconfirm bspwm nvidia amd-ucode gcc gvfs gvfs-mtp htop imagemagick kitty neofetch mpv lxappearance polkit-gnome qbittorrent rofi swappy dunst thunar thunar-volman thunar-archive-plugin fileroller unrar unzip  webp-pixbuf-loader pipewire wireplumber pipewire-pulse xclip yt-dlp zsh steam ttf-jetbrains-mono-nerd man pavucontrol firefox plocate tumbler feh bat eza starship picom-ftlabs-git sxhkd npm polybar  
 
 # zsh
 echo "Configuring zsh"
@@ -45,7 +45,8 @@ git clone https://github.com/ltlis/dots
 cd dots
 
 cp -r config $HOME/.config
-cp -r .zshrc  $HOME/
+cp -r .zshrc $HOME/
+cp -r .fonts $HOME/
 
 echo "Installation complete"
 echo
